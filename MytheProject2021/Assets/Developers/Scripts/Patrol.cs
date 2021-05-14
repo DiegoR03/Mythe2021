fd47ce3a150c99fb1cd75ef9b267a9b6542ce145
29244 <29244@ma-web.nl>
2021-05-14 16:31:04 +0200

    AImovement

---
+++ MytheProject2021/Assets/Developers/Scripts/Patrol.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    public float speed;
+    private float waitTime;
+    public float startWaitTime;
+
+    public Transform[] moveSpots;
+    private int randomSpot;
+
+    void Start()
+    {
+        waitTime = startWaitTime;
+        randomSpot = Random.Range(0, moveSpots.Length);
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+
+        if(Vector3.Distance(transform.position, moveSpots[randomSpot].position) < 0.2)
+        {
+            if(waitTime <= 0)
+            {
+                randomSpot = Random.Range(0, moveSpots.Length);
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+}
