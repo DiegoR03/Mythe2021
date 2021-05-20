fc1a1e5bffe204af248515ec4099e2d92c78a333
29244 <29244@ma-web.nl>
2021-05-20 16:21:24 +0200

    MainMenu.1

---
+++ MytheProject2021/Assets/Developers/Scripts/UI Scripts/LevelSelect.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    public string level;
+
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene(level);
+    }
+}
